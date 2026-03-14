473c97dfe008ba9e62bcc360ed55a5bd993663a0
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-14 21:50:59 +0100

    preparation

---
+++ astro.config.vps.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node"; // Garde bien celui-là
+// Retire l'import netlify s'il n'est plus utile
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        ignored: [
+          "**/node_modules/**",
+          "**/backend/**",
+          "**/public/360/**",
+          "**/.git/**",
+        ],
+      },
+    },
+  },
+  site: "https://expo.mmimontbeliard.com",
+  output: "server", // C'est parfait
+  integrations: [sitemap()],
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pb-expo.nicolas-thai.fr",
+      },
+    ],
+    domains: ["127.0.0.1", "localhost"],
+  },
+  // --- MODIFICATION ICI ---
+  adapter: node({
+    mode: "standalone",
+  }),
+  // -------------------------
+});
