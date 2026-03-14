50709e459cbe8b25c43a191eb27b8e1e9ad08866
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-14 22:12:49 +0100

    test

--- astro.config.vps.mjs
+++ astro.config.vps.mjs
@@ -27,7 +27,7 @@ export default defineConfig({
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pb-expo.nicolas-thai.fr",
+        hostname: "mmiart26.nicolas-thai.fr",
       },
     ],
     domains: ["127.0.0.1", "localhost"],
